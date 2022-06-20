2b263851fa1f3fa8765ff2f908ba52567d2ed102
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-19 23:38:02 -0400

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Miguel Quezada on 19-06-22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
