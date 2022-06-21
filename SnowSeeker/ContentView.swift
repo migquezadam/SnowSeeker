053275888dac824f08577065c46fea27f33d6004
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-20 22:04:50 -0400

    Complete app

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,13 +7,72 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+extension View{
+    @ViewBuilder func phoneOnlyNavigationView() -> some View{
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            self.navigationViewStyle(.stack)
+        } else {
+            self
+        }
     }
 }
 
+
+struct ContentView: View {
+    
+    @State private var searchText = ""
+    @StateObject var favorites = Favorites()
+
+    
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    var body: some View {
+        NavigationView {
+            List(resorts){ resort in
+                NavigationLink{
+                    ResortView(resort: resort)
+                } label: {
+                    HStack{
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth:  1)
+                            )
+                        VStack(alignment: .leading){
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                        }
+                        if favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                                .accessibilityLabel("This is a favorite resort")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
+            .searchable(text: $searchText, prompt: "Search for a resort")
+            
+            WelcomeView()
+        }
+        .phoneOnlyNavigationView()
+        .environmentObject(favorites)
+
+    }
+    var filteredResorts: [Resort] {
+        if searchText.isEmpty{
+            return resorts
+        } else {
+            return resorts.filter {$0.name.localizedCaseInsensitiveContains(searchText)}
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
